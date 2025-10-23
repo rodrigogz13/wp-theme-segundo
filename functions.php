54c2e2e63bb2efb91b8711e882756c56837d77e5
rodrigogz13 <rgutierrez505@uvaq.edu.mx>
2025-10-22 20:18:14 -0600

    primeros paso

---
+++ functions.php
@@ -0,0 +1,5 @@
+<?php
+    register_nav_menus( array(
+        'primary' => __('menu principal'),
+        'footer' => __('menu del pie'),
+    ) );
---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/mystyle.css">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/css/all.css">
+</head>
+<body class= "">
+
+<?php wp_nav_menu( array( 'theme_location' => 'primary' ));?>
